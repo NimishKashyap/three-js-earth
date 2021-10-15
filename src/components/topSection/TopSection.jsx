edf0215f9c191f61144bd3eb3c725c56ba77d8ae
NimishKashyap <kashap.nimish8@gmail.com>
2021-10-15 11:53:18 +0530

    Finished Earth

---
+++ src/components/topSection/TopSection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const TopSectionContainer = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: #1756dd81;
+    display: flex;
+    flex-direction: column;
+    aligh-items: center;
+`;
+const Logo = styled.h1`
+    margin: 0;
+    color: #fff;
+    font-weight: 700;
+    font-size: 35px;
+`;
+const Slogain = styled.h4`
+    margin: 0;
+    color: #fff;
+    font-weight: 700;
+    font-size: 20px;
+    margin-top: 1rem;
+`;
+function TopSection() {
+    return <div></div>;
+}
+
+export default TopSection;
